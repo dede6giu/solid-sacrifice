e552276ed6a3c4d6f9eb2cbec4be68587636d6a2
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-11-04 21:48:06 -0300

    player base animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,18 +1,26 @@
 extends CharacterBody2D
 
-
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED := 130.0
+const JUMP_VELOCITY := -300.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var isJump := false
+var isFreeFall := false
+@onready var free_fall_timer: Timer = $FreeFallTimer
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	else:
+		isJump = false
+		isFreeFall = false
+		free_fall_timer.stop()
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite_2d.play("Jump")
+		isJump = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,13 +29,29 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.flip_h = false
 	elif direction < 0: 
 		animated_sprite_2d.flip_h = true
-	if direction == 0:
-		animated_sprite_2d.play("Idle")
-	else:
-		animated_sprite_2d.play("Walk")
+	
+	if !isJump:
+		if direction == 0:
+			animated_sprite_2d.play("Idle")
+		else:
+			animated_sprite_2d.play("Walk")
+	elif velocity.y > 0 and !isFreeFall:
+		print("freefall entered")
+		isFreeFall = true
+		animated_sprite_2d.play("FreeFall_A")
+		free_fall_timer.start()
+		
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_free_fall_timer_timeout() -> void:
+	print("freefall timer run out")
+	if !is_on_floor():
+		animated_sprite_2d.play("FreeFall_B")
