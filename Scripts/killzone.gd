07ebb0909606b122500b96f36b827f0a369906c6
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-11-05 22:45:22 -0300

    Revert "Merge branch 'main' of https://github.com/dede6giu/gamejam-unb-2024"
    This reverts commit 1c0ca3a309f5b1e1b931224aecd830744b20aa89, reversing
    changes made to 3d9825e571553a247d5b1acfc4ef21257346b8fc.

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,35 +1,5 @@
 extends Area2D
 @onready var timer = $Timer
-<<<<<<< HEAD
-@onready var player = get_parent().get_node("Player") 
-var inicial_player_pos = Vector2()
-var statuePath = ResourceLoader.load("res://Scenes/statue.tscn")
-
-func _ready():
-	if player:
-		inicial_player_pos = player.global_position
-		print("Pos inicial: ", inicial_player_pos)
-		
-func create_statue(position: Vector2):
-	var newStatue = statuePath.instantiate()
-	newStatue.position = position
-	add_child(newStatue)
-	print("Spawning at :", position)
-
-func _on_body_entered(body: Node2D) -> void:
-	print("Check player exists ", player)
-	if body.name == "Player":
-		print("You died")
-		timer.start()
-		create_statue(body.position)
-	
-func _on_timer_timeout() -> void:
-	if player:
-		print("Current position: ", player.global_position)
-		player.global_position = inicial_player_pos
-		print("Returning on ", player.global_position)
-	
-=======
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
@@ -37,4 +7,3 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
->>>>>>> 2597bb9a33d64c60c0c1dee28731f12553351ca2
