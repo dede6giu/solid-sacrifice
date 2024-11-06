06889286b8dbf4800287ea368cff9003761149e2
Carlos Cauã <137356546+Schatten900@users.noreply.github.com>
2024-11-06 09:22:06 -0300

    Update killzone.gd

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,29 +1,33 @@
 extends Area2D
 @onready var timer = $Timer
-@onready var player = get_parent().get_node("Player") 
+var player = null
 var inicial_player_pos = Vector2()
-var statuePath = ResourceLoader.load("res://Scenes/statue.tscn")
+@onready var spawn = get_parent().get_parent().get_parent().get_node("SpawnPoint")
+var statuePath = preload("res://Scenes/statue.tscn")
 
 func _ready():
-	if player:
-		inicial_player_pos = player.global_position
-		print("Pos inicial: ", inicial_player_pos)
-		
-func create_statue(position: Vector2):
+	print(spawn)
+
+func create_statue(character_position: Vector2):
 	var newStatue = statuePath.instantiate()
-	newStatue.position = position
+	print("aaaaaaa")
+	newStatue.position = character_position - get_parent().position
 	add_child(newStatue)
-	print("Spawning at :", position)
+	print("Spawning at :", character_position)
 
 func _on_body_entered(body: Node2D) -> void:
+	inicial_player_pos = spawn.global_position
+	player = body
 	print("Check player exists ", player)
-	if body.name == "Player":
-		print("You died")
-		timer.start()
-		create_statue(body.position)
+	print("You died")
+	timer.start()
+	print("Player died on ", player.global_position)
+	#body.get().animated_sprite_2d.play("Death")
+	create_statue(player.global_position)
 	
 func _on_timer_timeout() -> void:
 	if player:
 		print("Current position: ", player.global_position)
-		player.global_position = inicial_player_pos
-		print("Returning on ", player.global_position)
+		player.position = inicial_player_pos
+		print("Respawning on ", player.global_position)
+		player = null
