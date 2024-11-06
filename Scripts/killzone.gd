f41ee4e72f023005f37b74c8aaefbfccd1779a10
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-11-06 08:14:46 -0300

    death box

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,9 +1,29 @@
 extends Area2D
 @onready var timer = $Timer
+@onready var player = get_parent().get_node("Player") 
+var inicial_player_pos = Vector2()
+var statuePath = ResourceLoader.load("res://Scenes/statue.tscn")
+
+func _ready():
+	if player:
+		inicial_player_pos = player.global_position
+		print("Pos inicial: ", inicial_player_pos)
+		
+func create_statue(position: Vector2):
+	var newStatue = statuePath.instantiate()
+	newStatue.position = position
+	add_child(newStatue)
+	print("Spawning at :", position)
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You died!")
-	timer.start()
-
+	print("Check player exists ", player)
+	if body.name == "Player":
+		print("You died")
+		timer.start()
+		create_statue(body.position)
+	
 func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene()
+	if player:
+		print("Current position: ", player.global_position)
+		player.global_position = inicial_player_pos
+		print("Returning on ", player.global_position)
