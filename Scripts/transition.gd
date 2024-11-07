95d06dd46a2bd9c70cf4f672501d0bf083e2f80e
Luke0133 <lucahmeg@gmail.com>
2024-11-07 12:25:44 -0300

    Transitions :D

--- Scripts/transition.gd
+++ Scripts/transition.gd
@@ -1,24 +1,30 @@
 extends Area2D
 
-@export var path = "res://Scenes/teste.tscn"  
+@export var path = "res://Scenes/game.tscn"  
 
 var player = null  
 
-
 func _on_body_entered(body: Node2D) -> void:
 	player = body
-	
-	var current_level = get_parent().get_parent()   
-	
-	var next_level = ResourceLoader.load(path).instantiate()
-	
-	var pos = next_level.get_node("SpawnPoint").position
-
-	Global.save_position_for_scene(str(current_level.get_instance_id()), pos)
-
-	current_level.get_parent().add_child(next_level)
-	
-	current_level.queue_free()
+	TransitionAnimation.changeScene("RightToLeft")
+	player.isDead = true
 
 func _on_body_exited(body: Node2D) -> void:
 	player = null
+
+func _process(delta: float) -> void:
+	if (TransitionAnimation.animated_transition.current_animation == "RightToLeft" and TransitionAnimation.animated_transition.current_animation_position > 1.4):
+		player.isDead = false
+		var current_level = get_parent().get_parent()   
+		
+		var next_level = ResourceLoader.load(path).instantiate()
+		
+		var pos = next_level.get_node("SpawnPoint").position
+
+		Global.save_position_for_scene(str(current_level.get_instance_id()), pos)
+
+		current_level.get_parent().add_child(next_level)
+		
+		current_level.queue_free()
+		
+		TransitionAnimation.changeScene("OpenToLeft")
---
+++ Scripts/transitionAnimation.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+@onready var animated_transition: AnimationPlayer = $AnimatedTransition
+
+
+# Called when the node enters the scene tree for the first time.
+func changeScene(type : String = "RightToLeft"):
+	if type == "RightToLeft":
+		transition_RightToLeft()
+	else:
+		transition_OpenToLeft()
+		
+func transition_RightToLeft():
+	animated_transition.play("RightToLeft")
+
+func transition_OpenToLeft():
+	animated_transition.play("OpenToLeft")
+	await animated_transition.animation_finished
