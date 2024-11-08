9c4dc29c358c04b88139c22dea75c286f028f6a5
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-11-07 21:51:39 -0300

    timer for screen

--- Scripts/titlescreen.gd
+++ Scripts/titlescreen.gd
@@ -1,12 +1,29 @@
 extends Control
 @onready var statue_change: Timer = $"Statue Change"
-var timer
+var timerStatus := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	timerStatus = 0
+	statue_change.wait_time = randf_range(7, 15)
+	statue_change.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_statue_change_timeout() -> void:
+	if timerStatus == 0:
+		# case normal    -> halfstone
+		print(timerStatus)
+		statue_change.wait_time = randf_range(1, 3)
+		statue_change.start()
+		timerStatus = 1
+	elif timerStatus == 1:
+		# case halfstone -> statue
+		print(timerStatus)
+		statue_change.wait_time = randf_range(2, 7)
+		statue_change.start()
+		timerStatus = 2
+	else:
+		# case statue    -> normal
+		print(timerStatus)
+		statue_change.wait_time = randf_range(7, 15)
+		statue_change.start()
+		timerStatus = 0
