cce33c1cb82c77df73198d25f26f19cc791011b6
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-11-07 21:32:42 -0300

    timer for statue change

---
+++ Scripts/titlescreen.gd
@@ -0,0 +1,12 @@
+extends Control
+@onready var statue_change: Timer = $"Statue Change"
+var timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
